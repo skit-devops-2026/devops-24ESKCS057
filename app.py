a6d4d100aa5c82f250b328b6275eea9ff9a93395
Arnav Agrawal <arnavagrawalsp@gmail.com>
2026-09-11 00:13:49 +0530

    Add product endpoint

--- app.py
+++ app.py
@@ -15,5 +15,14 @@ def health():
     })
 
 
+@app.get("/products")
+def products():
+    return jsonify([
+        {"id": 1, "name": "Laptop", "price": 55000},
+        {"id": 2, "name": "Headphones", "price": 2500},
+        {"id": 3, "name": "Keyboard", "price": 1500}
+    ])
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
